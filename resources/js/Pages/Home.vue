f750a2d089d46fd812ea07d486e1e74df35da913
Rafael de Abreu <rafael.abreu.ferreira10@gmail.com>
2025-07-10 14:38:49 -0300

    first commit. setting laravel, vue and inertiajs configurations

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="py-5">
+      <h1 class="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    </div>
+</template>
---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import tailwindcss from '@tailwindcss/vite';
+import vue from "@vitejs/plugin-vue"
+import tailwindcss from '@tailwindcss/vite'
+
+export default defineConfig({
+    plugins: [
+        vue(),
+        tailwindcss(),
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+            refresh: true,
+        }),
+        tailwindcss(),
+    ],
+});
